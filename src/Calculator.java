a66657f0543d462c425ccfce35324e08fc47e8d6
Jhon Sebastian Martinez Soto <jhon.martinez@globant.com>
2021-06-09 13:52:35 -0500

    create method to sum and subtract numbers

---
+++ src/Calculator.java
@@ -0,0 +1,10 @@
+public double basicOperations(double num1, double num2, int option){}
+    double result = 0;
+    if(option == 1){
+        result = num1 + num2;
+    }
+    else {
+        result = num1 - num2;
+    }
+    return result;
+}
