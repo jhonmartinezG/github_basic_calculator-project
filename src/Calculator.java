d15ea7a3394a54709fce702d50a0a97dad6a612b
Jhon Sebastian Martinez Soto <jhon.martinez@globant.com>
2021-06-09 22:45:51 -0500

    create funtions for the advanced operations

---
+++ src/Calculator.java
@@ -0,0 +1,9 @@
+public double advancedOperations(double num1, double num2, int option){
+    double result = 0;
+    if(opt == 1) {
+        return result = num1 * num2;
+    }
+    else {
+      return result = num1 / num2;
+    }
+}
